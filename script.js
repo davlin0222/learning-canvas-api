c81cc04f3dda6b4c07a266f89dd6076713c0810f
David Lindström <davlin@creatorise.com>
2024-05-20 20:20:45 +0200

    Add test making canvas distorted
    If your renderings seem distorted, try specifying your width and height attributes explicitly in the <canvas> attributes, and not using CSS.

--- script.js
+++ script.js
@@ -4,13 +4,8 @@ const canvas = document.getElementById("canvas");
 // Get the 2D rendering context
 const ctx = canvas.getContext("2d");
 
-//? Active question: How do I make the green rectangle take up all the space?
-
 // Set fill color to green
 ctx.fillStyle = "green";
 
-// Draw a filled rectangle that covers the entire canvas:
-// - top-left corner at (0, 0)
-// - width of the canvas
-// - height of the canvas
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+// Fill the entire canvas with the green rectangle
+ctx.fillRect(10, 10, 100, 100);
